8ff0f008beb40b14ed53c4d1642428492767e851
Abhishek <Abhishek@DESKTOP-5VJGJ96>
2024-07-16 17:33:25 +0530

    Service INTERFACE Setup

---
+++ src/main/java/com/bk/code/service/ADOService.java
@@ -0,0 +1,12 @@
+package com.bk.code.service;
+
+public interface ADOService {
+	
+	public void connectDB();
+	public boolean verifyLogin(String email, String Password);
+	public void addRegistation(String name,String email,String moibile);
+	public void deleteRegistation(String email);
+	public void updateRegistation(String email,String phone);
+	public void getAllRegistations();
+	
+}
